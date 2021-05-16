5e1c8082bcf3466e3e12f6794c7ad5b9ff3a49bf
Yanis <54286672+yanbxr@users.noreply.github.com>
2021-05-17 01:42:13 +0200

    Update d2.sh

--- d2.sh
+++ d2.sh
@@ -80,7 +80,7 @@ install_dependencies () {
 
 setup () {
   echo -e "${BLUE}Script by ${RED}[Yanis]"
-  echo -e "${GREEN}Setting up firewall rules"
+  echo -e "${GREEN}Setting up firewall rules${NC}"
   reset_ip_tables
   read -p "Enter your platform ([xbox], [psn] or [steam]): " platform
   platform=$(echo "$platform" | xargs)
